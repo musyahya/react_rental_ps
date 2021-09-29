950dcbaf9f942eb6b9ed801d07e482e56a8bc052
musyahya <mananabdul37654@gmail.com>
2021-09-29 23:10:16 +0700

    regsiter

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,35 +1,73 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState} from 'react'
 import { Row, Button, Col, Form, Card } from "react-bootstrap";
 
 function Register() {
+
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    const [passwordConfirmation, setPasswordConfirmation] = useState();
+
+    function register() {
+        axios
+          .post("http://127.0.0.1:8000/api/register", {
+            name: name,
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation,
+          })
+          .then(function (response) {
+            console.log(response);
+             localStorage.setItem("token", response.data.token);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
     return (
       <Row className="mt-5 justify-content-center">
         <Col md={6}>
-          <Card>
-            <Card.Header className="text-center bg-primary text-white">Halaman Register</Card.Header>
+          <Card className="shadow border-radius">
+            <Card.Header className="text-center bg-primary text-white header-radius">
+              Halaman Register
+            </Card.Header>
             <Card.Body>
               <Form>
                 <Form.Group className="mb-3">
                   <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" />
+                  <Form.Control
+                    onChange={(e) => setName(e.target.value)}
+                    type="text"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control type="email" />
+                  <Form.Control
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" />
+                  <Form.Control
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                  />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                   <Form.Label>Konfirmasi Password</Form.Label>
-                  <Form.Control type="password" />
+                  <Form.Control
+                    onChange={(e) => setPasswordConfirmation(e.target.value)}
+                    type="password"
+                  />
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Button variant="primary" onClick={register}>
                   Register
                 </Button>
               </Form>
