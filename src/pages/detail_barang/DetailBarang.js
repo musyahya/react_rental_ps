1e7e13f57fe3bd4b13e088a612fceecc653e3ab0
musyahya <mananabdul37654@gmail.com>
2021-10-01 22:24:19 +0700

    tambah detail barang

--- src/pages/detail_barang/DetailBarang.js
+++ src/pages/detail_barang/DetailBarang.js
@@ -2,14 +2,52 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import SidebarComponent from '../../components/SidebarComponent';
 import TableComponent from "../../components/TableComponent";
+import { Button, Form, Modal } from "react-bootstrap";
 
 function DetailBarang(props) {
 
     const [detailBarang, setDetailBarang] = useState()
+    const [barang, setBarang] = useState()
+
+    const [barangId, setBarangId] = useState()
+    const [harga, setHarga] = useState()
+    const [durasi, setDurasi] = useState()
+
+     const [tambah, setTambah] = useState();
+
+     const [show, setShow] = useState(false);
+     const handleShow = () => setShow(true);
+
+     function handleClose () {
+         setShow(false)
+         setHarga(" ")
+         setDurasi(" ")
+         setBarangId(" ")
+         setTambah(false)
+     }
+
+    useEffect(() => {
+        getBarang()
+    }, [])
 
     useEffect(() => {
         getDetailBarang()
     }, [])
+
+     function getBarang() {
+       axios({
+         method: "get",
+         url: "http://127.0.0.1:8000/api/barang",
+         headers: { Authorization: `Bearer ${props.token}` },
+       })
+         .then(function (response) {
+           console.log(response);
+           setBarang(response.data.data);
+         })
+         .catch(function (error) {
+           console.log(error);
+         });
+     }
 
     function getDetailBarang() {
           axios({
@@ -26,9 +64,92 @@ function DetailBarang(props) {
             });
     }
 
+    function postDetailBarang() {
+        axios({
+          method: "post",
+          url: "http://127.0.0.1:8000/api/detail_barang",
+          headers: { Authorization: `Bearer ${props.token}` },
+          data: {
+              barang_id: barangId,
+              harga: harga,
+              durasi: durasi
+          }
+        })
+          .then(function (response) {
+            console.log(response);
+            handleClose();
+            getDetailBarang();
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
+
+    function showTambah() {
+        handleShow()
+        setTambah(true)
+    }
+    
     return (
       <SidebarComponent>
         <h1>Detail Barang</h1>
+
+        <Button
+          variant="primary"
+          size="sm"
+          className="mt-3"
+          onClick={showTambah}
+        >
+          Tambah
+        </Button>
+
+        <Modal show={show} onHide={handleClose} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>{tambah && "Tambah"} Detail Barang</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Barang</Form.Label>
+                <Form.Select onChange={(e) => setBarangId(e.target.value)}>
+                  <option>Pilih Barang</option>
+                  {barang &&
+                    barang.map((barang) => (
+                      <option value={barang.id}>{barang.nama}</option>
+                    ))}
+                </Form.Select>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Harga</Form.Label>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  onChange={(e) => setHarga(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Durasi</Form.Label>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  onChange={(e) => setDurasi(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Batal
+            </Button>
+            {tambah && (
+              <Button variant="primary" onClick={postDetailBarang}>
+                Simpan
+              </Button>
+            )}
+          </Modal.Footer>
+        </Modal>
 
         <TableComponent>
           <thead>
