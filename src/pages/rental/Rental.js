d81df036f975e264e23a5401fe3f65bfe2e8d478
musyahya <mananabdul37654@gmail.com>
2021-10-05 18:35:02 +0700

    update rental

--- src/pages/rental/Rental.js
+++ src/pages/rental/Rental.js
@@ -4,13 +4,22 @@ import { useHistory } from "react-router-dom";
 import SidebarComponent from '../../components/SidebarComponent'
 import TableComponent from "../../components/TableComponent";
 import { API_URL } from "../../utility/Url";
-import { Button } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 
 function Rental(props) {
 
      const history = useHistory();
 
      const [rental, setRental] = useState()
+     const [nama, setNama] = useState()
+     const [email, setEmail] = useState()
+     const [hp, setHp] = useState()
+     const [alamat, setAlamat] = useState()
+
+      const [show, setShow] = useState(false);
+
+      const handleClose = () => setShow(false);
+      const handleShow = () => setShow(true);
 
      useEffect(() => {
        cekRole();
@@ -34,15 +43,100 @@ function Rental(props) {
            .then(function (response) {
              console.log(response);
              setRental(response.data.data);
+             setNama(response.data.data.nama)
+             setEmail(response.data.data.email)
+             setHp(response.data.data.hp)
+             setAlamat(response.data.data.alamat);
            })
            .catch(function (error) {
              console.log(error);
            });
      }
 
+     function updateRental() {
+         axios({
+           method: "put",
+           url: API_URL + "rental",
+           headers: { Authorization: `Bearer ${props.token}` },
+           data: {
+               nama: nama,
+               email: email,
+               hp: hp,
+               alamat: alamat
+           }
+         })
+           .then(function (response) {
+             console.log(response);
+             getRental()
+              setNama(" ");
+              setEmail(" ");
+              setHp(" ");
+              setAlamat(" ");
+              handleClose()
+           })
+           .catch(function (error) {
+             console.log(error.response);
+           });
+     }
+
     return (
       <SidebarComponent>
         <h1>Rental</h1>
+
+        <Modal show={show} onHide={handleClose} size="lg">
+          <Modal.Header closeButton>
+            <Modal.Title>Edit Rental</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3">
+                <Form.Label>Nama</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={nama}
+                  onChange={(e) => setNama(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>HP</Form.Label>
+                <Form.Control
+                  type="number"
+                  min="1"
+                  value={hp}
+                  onChange={(e) => setHp(e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Alamat</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  rows={3}
+                  value={alamat}
+                  onChange={(e) => setAlamat(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Batal
+            </Button>
+            <Button variant="primary" onClick={updateRental}>
+              Update
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
         <TableComponent>
           <thead>
@@ -62,14 +156,14 @@ function Rental(props) {
                 <td>{rental.hp}</td>
                 <td>{rental.alamat}</td>
                 <td>
-                    <Button
-                      size="sm"
-                    //   onClick={() => getBarangId(barang.id)}
-                      className="mx-2"
-                      variant="primary"
-                    >
-                      Edit
-                    </Button>
+                  <Button
+                    size="sm"
+                    onClick={handleShow}
+                    className="mx-2"
+                    variant="primary"
+                  >
+                    Edit
+                  </Button>
                 </td>
               </tr>
             )}
